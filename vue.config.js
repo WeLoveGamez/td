aca8406ff63a3f2ffc6e63bc6eb616e0a018dd3d
Matthes Staacken <m.staacken@mbdteam.onmicrosoft.com>
2022-03-03 09:35:11 +0100

    build

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    publicPath: '/td',
+    outputDir: './docs',
+    //pwa: {
+    //workboxPluginMode: "InjectManifest",
+    //workboxOptions: {
+    //swSrc: "src/service-worker.js",
+    //},
+    //},
+    css: {
+        extract: false,
+    },
+}
